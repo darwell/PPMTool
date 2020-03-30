07360f9b8725d22be521092bea62fbc1d2224579
Darko <darusohrid@yahoo.com>
2020-03-30 21:49:23 +0100

    first commit

---
+++ ppmtool/src/main/java/com/practice/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,15 @@
+package com.practice.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+		
+		System.out.println("HELLLLOOOOOO!!!!");
+	}
+
+}
